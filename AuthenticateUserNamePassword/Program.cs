0a100cc4e4d4f66200df19296f44a333e96e46ee
Edward-Zhou <v-tazho@hotmail.com>
2016-05-16 19:26:23 +0800

    authenticate with user name ans passowrd

---
+++ AuthenticateUserNamePassword/Program.cs
@@ -0,0 +1,42 @@
+﻿using AuthenticateUserNamePassword.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticateUserNamePassword
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string username, password;
+            GetPassword(out username,out password);
+
+            Service1Client client = new Service1Client();
+            //set username and password
+            client.ClientCredentials.UserName.UserName = username;
+            client.ClientCredentials.UserName.Password = password;
+
+            //treat the test certificate as trusted
+            client.ClientCredentials.ServiceCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.PeerOrChainTrust;
+
+
+            string result=client.GetData(123);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public static void GetPassword(out string username,out string password)
+        {
+            Console.WriteLine("Provide a valid machine or domain account. [domain\\user]");
+            Console.WriteLine("   Enter username:");
+            username = Console.ReadLine();
+            Console.WriteLine("   Enter password:");
+            password = Console.ReadLine();
+            return;
+
+        }
+    }
+}
---
+++ AuthenticateUserNamePasswordServiceHost/Program.cs
@@ -0,0 +1,50 @@
+﻿using AuthenticateUserNamePasswordServiceLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticateUserNamePasswordServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/AuthenticateUserNamePassword/Service");
+            ServiceHost selfhost = new ServiceHost(typeof(Service1),baseAddress);
+            try
+            {
+                //add username and password 
+                WSHttpBinding wsbinding = new WSHttpBinding();
+                wsbinding.Security.Mode = SecurityMode.Message;
+                wsbinding.Security.Message.ClientCredentialType = MessageCredentialType.UserName;
+
+                selfhost.AddServiceEndpoint(typeof(IService1), wsbinding, "AuthenticateUserNamePassword");
+
+                selfhost.Credentials.ServiceCertificate.SetCertificate(StoreLocation.LocalMachine,StoreName.My,X509FindType.FindBySubjectName,"localhost");
+                
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfhost.Description.Behaviors.Add(smb);
+                selfhost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.
+                selfhost.Close();
+            }
+            catch (Exception ce)
+            {
+
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+
+            }
+        }
+    }
+}
