adddd3956b9ff6ece7cb76a4a4a2fa4b37dbe879
Edward-Zhou <v-tazho@hotmail.com>
2016-05-16 21:39:00 +0800

    wcf web service and host in iis

---
+++ WcfSelfHostWithAsyncMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using WcfSelfHostWithAsyncMethod.ServiceReference1;
+
+namespace WcfCallWithAsyncMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://localhost:42797/Service1.svc");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            Service1Client client = new Service1Client(binding,new EndpointAddress(uri));
+            string result = client.TestAsyncMethod();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
