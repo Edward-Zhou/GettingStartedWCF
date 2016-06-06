67775d5e5b141b62722e6a3fc9b7dcc873f9e4fe
Edward-Zhou <v-tazho@hotmail.com>
2016-06-06 17:46:19 +0800

    retrieve metadata

---
+++ DownloadMetadataMetadataExchangeClient/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using DownloadMetadataMetadataExchangeClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadMetadataMetadataExchangeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("URI of the metadata documetns retreived");
+            //MetadataExchangeClient metaTransfer = new MetadataExchangeClient(new Uri("http://localhost:1169/Service1.svc/mex"), MetadataExchangeClientMode.MetadataExchange);
+            MetadataExchangeClient metaTransfer = new MetadataExchangeClient(new Uri("http://localhost:1169/Service1.svc"), MetadataExchangeClientMode.HttpGet);
+            metaTransfer.ResolveMetadataReferences = true;
+            MetadataSet metadata = metaTransfer.GetMetadata();
+            foreach (MetadataSection doc in metadata.MetadataSections)
+                Console.WriteLine(doc.Dialect + " : " + doc.Metadata);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DownloadMetadataMetadataResolver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using DownloadMetadataMetadataResolver.ServiceReference1;
+
+namespace DownloadMetadataMetadataResolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EndpointAddress metaAddress = new EndpointAddress(new Uri("http://localhost:1169/Service1.svc/mex"));
+            ServiceEndpointCollection endpoints = MetadataResolver.Resolve(typeof(IService1), metaAddress);
+            foreach (ServiceEndpoint point in endpoints)
+            {
+                if (point != null)
+                {
+                    using (Service1Client wcfClient = new Service1Client(point.Binding, point.Address))
+                    {
+                        Console.WriteLine(wcfClient.GetString(String.Format("binding is {0}, address is {1}",point.Binding,point.Address)));
+                    }
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
